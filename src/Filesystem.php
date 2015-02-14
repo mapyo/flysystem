ca3b098eb7fcec9e074e426f34b6e7c550d80f27
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2015-02-15 00:56:59 +0900

    fix trait in Filesystem

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -7,6 +7,8 @@ use InvalidArgumentException;
 use League\Flysystem\Plugin\PluggableTrait;
 use League\Flysystem\Plugin\PluginNotFoundException;
 
+use LogicException;
+
 /**
  * @method array getWithMetadata(string $path, array $metadata)
  * @method array listFiles(string $path = '', boolean $recursive = false)
@@ -15,8 +17,6 @@ use League\Flysystem\Plugin\PluginNotFoundException;
  */
 class Filesystem implements FilesystemInterface
 {
-    use PluggableTrait;
-
     /**
      * @var AdapterInterface
      */
@@ -26,6 +26,11 @@ class Filesystem implements FilesystemInterface
      * @var Config
      */
     protected $config;
+
+    /**
+     * @var array
+     */
+    protected $plugins = array();
 
     /**
      * Constructor.
@@ -569,5 +574,58 @@ class Filesystem implements FilesystemInterface
                 .'::'.$method
             );
         }
+    }
+
+    /**
+     * Register a plugin.
+     *
+     * @param PluginInterface $plugin
+     *
+     * @return $this
+     */
+    public function addPlugin(PluginInterface $plugin)
+    {
+        $this->plugins[$plugin->getMethod()] = $plugin;
+
+        return $this;
+    }
+
+    /**
+     * Invoke a plugin by method name.
+     *
+     * @param string $method
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    protected function invokePlugin($method, array $arguments, FilesystemInterface $filesystem)
+    {
+        $plugin = $this->findPlugin($method);
+        $plugin->setFilesystem($filesystem);
+        $callback = array($plugin, 'handle');
+
+        return call_user_func_array($callback, $arguments);
+    }
+
+    /**
+     * Register a plugin.
+     *
+     * @param string $method
+     *
+     * @throws LogicException
+     *
+     * @return PluginInterface $plugin
+     */
+    protected function findPlugin($method)
+    {
+        if (! isset($this->plugins[$method])) {
+            throw new PluginNotFoundException('Plugin not found for method: '.$method);
+        }
+
+        if (! method_exists($this->plugins[$method], 'handle')) {
+            throw new LogicException(get_class($this->plugins[$method]).' does not have a handle method.');
+        }
+
+        return $this->plugins[$method];
     }
 }
