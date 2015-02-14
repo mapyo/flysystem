e3d758e3ce53f3a438d40cdcfb8037ff67b8282e
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2015-02-15 01:17:54 +0900

    fix StreamedReadingStub

--- stub/StreamedReadingStub.php
+++ stub/StreamedReadingStub.php
@@ -6,14 +6,34 @@ use League\Flysystem\Adapter\Polyfill\StreamedReadingTrait;
 
 class StreamedReadingStub
 {
-    use StreamedReadingTrait;
-
     public function read($path)
     {
         if ($path === 'true.ext') {
-            return ['contents' => $path];
+            return array('contents' => $path);
         }
 
         return false;
+    }
+
+    /**
+     * Get the contents of a file in a stream.
+     *
+     * @param string $path
+     *
+     * @return resource|false false when not found, or a resource
+     */
+    public function readStream($path)
+    {
+        if (! $data = $this->read($path)) {
+            return false;
+        }
+
+        $stream = tmpfile();
+        fwrite($stream, $data['contents']);
+        rewind($stream);
+        $data['stream'] = $stream;
+        unset($data['contents']);
+
+        return $data;
     }
 }
